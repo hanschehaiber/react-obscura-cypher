033b6b2d9d1d953a2c960791bb7f108e6c422323
Hans Chehaiber <39745948+hanschehaiber@users.noreply.github.com>
2025-12-08 22:34:01 -0800

    Make sure its working on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import { obscuraCypher } from './cypher';
 import Message from './Message';
 
 function App() {
   const [count, setCount] = useState(0);
+  const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const shifted = obscuraCypher(ALPHABET);
 
   return (
     <>
       <h1>Testing Deployment 2</h1>
       <h1>Hans' Vite + React App</h1>
+      <div>{ALPHABET}</div>
+      <div>{shifted}</div>
       <div className="card">
         <Message></Message>
         <button onClick={() => setCount((count) => count + 1)}>
